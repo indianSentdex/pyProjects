b7b51ff65ed02919fe0862442f08368ad56688f9
indianSentdex <39662206+indianSentdex@users.noreply.github.com>
2018-05-27 08:43:51 +0530

    Add files via upload

---
+++ cancerDatasetAnalysis.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataSetPath = "G:\\Machine Learning Stuff\\breast-cancer-wisconsin.data.txt"
+
+
+df = pd.read_csv(dataSetPath)
+
+print(df.loc[0][1])s
+
+
+
+
